30743f307ebe422d16af0960750c37550ecd239b
Мария Романович <mariagalko.info@gmail.com>
2022-08-28 00:37:06 +0300

    Fix styles for button

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -15,7 +15,7 @@ export function Profile() {
             className="profile__button"
             target="_blank"
             rel="noopener noreferrer"
-            href="https://drive.google.com/file/d/1eODbWckO4B1mR8cypgfQxXwZbloLFRBD/view">
+            href="https://drive.google.com/file/d/19XRK_tFY0O3HBDFtawCWJn8pfaQEiw6R/view">
             Скачать резюме
           </a>
         </div>
