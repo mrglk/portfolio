c803a0f68f868cc9c0ee0f5c9a31704ac04591d2
mrglk <mariagalko.info@gmail.com>
2022-10-07 15:24:38 +0400

    Edit link for resume

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -9,13 +9,13 @@ export function Profile() {
       </div>
       <div className="profile__info">
         <h1 className="profile__name">Мария Галко</h1>
-        <p className="profile__title">Frontend-разработчица JS/React</p>
+        <p className="profile__title">Frontend-разработчик JS/React</p>
         <div className="profile__buttonWrapper">
           <a
             className="profile__button"
             target="_blank"
             rel="noopener noreferrer"
-            href="https://drive.google.com/file/d/1ODNguhEUdWvIDSfGaP0wmiFSyfiOzC4e/view">
+            href="https://drive.google.com/file/d/1A85MtM-R5M_bobYrPtEU5i01gyatTL-e/view">
             Скачать резюме
           </a>
         </div>
