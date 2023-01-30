c296421256b6ead74e5596bda6c850129c2e5901
mrglk <mariagalko.info@gmail.com>
2023-01-30 19:30:50 +0800

    New photo and link

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -15,7 +15,7 @@ export function Profile() {
             className="profile__button"
             target="_blank"
             rel="noopener noreferrer"
-            href="https://drive.google.com/file/d/1tzVyyqxNBpPaPcjgJxg78ye_Lbca3XEX/view">
+            href="https://drive.google.com/file/d/1TzM3BzhcPlvc6iQ1DyznSnmTSFDZqRtD/view">
             Скачать резюме
           </a>
         </div>
